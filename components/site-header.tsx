9a63e39c86cd3ab11ac171b5559044e309c862d9
Neye <arohchinenye895@gmail.com>
2025-09-02 11:21:49 +0100

    Utils Folder

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 import Link from "next/link"
 import { HeartPulse, Menu, X } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils/cn"
 import { usePathname } from "next/navigation"
 
 export function SiteHeader() {
